a459f8f19dd5d0e6a103598c16347539268b79c9
Ben-Hur Pessoa <benehurp@gmail.com>
2024-05-19 18:14:40 -0400

    feat: flux control challenge

---
+++ controle-fluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException extends Exception {
+  public static void main(String[] args) {
+    System.out.println("O 'parametroUm' é maior que o 'parametroDois'");
+  }
+}
